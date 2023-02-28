fc3bc9fa5a16e96cc8d4a975d692fef30e0c374b
MrLad01 <ladayodeji@gmail.com>
2023-02-28 23:04:31 +0100

    new commit

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom"
 import gof from "../images/Rectangle 3.png"
+import arrow from "../images/chevron-down.png"
+
 export default function HomeLayout() {
   return (
     <div className="home">
-      <div className="home-text">
-        <p className="first-text">Studying books, a long lasting way to changing lives</p>
-        <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
-        <div>
-          <NavLink to="learn-more">Learn More</NavLink>
-          <NavLink to="contact">Contact Us</NavLink>
+      <div className="home1">
+        <div className="home-text">
+          <p className="first-text">Studying books, a long lasting way to changing lives</p>
+          <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
+          <div>
+            <NavLink to="learn-more">Learn More</NavLink>
+            <NavLink to="contact">Contact Us</NavLink>
+          </div>
         </div>
+        <img src={gof} alt="A girl reading" />
       </div>
-      <img src={gof} alt="A girl reading" />
 
-
+      <NavLink to="services"><img src={arrow} alt="" /></NavLink>
       
     </div>
   )
