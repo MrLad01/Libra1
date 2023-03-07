384560e4c6ac23a6986ab423fec69c84f62f1dab
MrLad01 <ladayodeji@gmail.com>
2023-03-07 11:09:15 +0100

    update on sign up page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom"
 
 export default function SignUp() {
   return (
     <div>
-      <h2>Hi!</h2>
+      <img src="" alt="" />
+      <div className="background">
+            <h2>SIGN UP</h2>
+            <div className="inputs">
+                <label htmlFor=""> Email</label>
+                <input type="text" placeholder="Enter E-mail" />
+                <label htmlFor=""> Password</label>
+                <input type="text" placeholder="Enter Password" />
+                <label htmlFor=""> Confirm Password</label>
+                <input type="text" placeholder="Enter Password again" />
+                <h5><Link>Forgot password?</Link></h5>
+            </div>
+            <button>Sign Up</button>
+            <div>
+                <h5>Or login using</h5>
+                <div>
+                    <Link><i></i></Link>
+                    <Link><i></i></Link>
+                    <Link><i></i></Link>
+                </div>
+            </div>  
+            <h4>New member? <Link to="sign-up">Sign Up</Link></h4>
+      </div>
     </div>
   )
 }
