746c7a133e66f87a7d67f159090ccf039b7021b5
MrLad01 <ladayodeji@gmail.com>
2023-03-12 09:34:46 +0100

    styling of login and signup pages

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -6,26 +6,28 @@ export default function LogIn() {
     <div>
       <img src="" alt="" />
       <div className="background">
-            <h2>LOG IN</h2>
-            <Form>
-              <div className="inputs">
-                  <label htmlFor="email"> Email</label>
-                  <input type="text" placeholder="Enter E-mail" id="email" name="email" />
-                  <label htmlFor="password"> Password</label>
-                  <input type="text" placeholder="Enter Password" id="password" name="password" />
-                  <h5><Link>Forgot password?</Link></h5>
-              </div>
-              <button>Log In</button>
-            </Form>
-            <div>
-                <h5>Or login using</h5>
-                <div>
-                    <Link><i></i></Link>
-                    <Link><i></i></Link>
-                    <Link><i></i></Link>
+        <div className="page-content">
+              <h2>LOG IN</h2>
+              <Form>
+                <div className="inputs">
+                    <label htmlFor="email"> Email</label>
+                    <input type="text" placeholder="Enter E-mail" id="email" name="email" />
+                    <label htmlFor="password"> Password</label>
+                    <input type="text" placeholder="Enter Password" id="password" name="password" />
+                    <h5><Link>Forgot password?</Link></h5>
                 </div>
-            </div>  
-            <h4>New member? <Link to="/sign-up">Sign Up</Link></h4>
+                <button>Log In</button>
+              </Form>
+              <div>
+                  <h5>Or login using</h5>
+                  <div>
+                      <Link><i></i></Link>
+                      <Link><i></i></Link>
+                      <Link><i></i></Link>
+                  </div>
+              </div>  
+              <h4>New member? <Link to="/sign-up">Sign Up</Link></h4>
+        </div>
       </div>
     </div>
   )
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,28 +5,30 @@ export default function SignUp() {
     <div>
       <img src="" alt="" />
       <div className="background">
-            <h2>SIGN UP</h2>
-            <Form>
-              <div className="inputs">
-                  <label htmlFor="email"> Email</label>
-                  <input type="text" placeholder="Enter E-mail" id="email" name="email" />
-                  <label htmlFor="password"> Password</label>
-                  <input type="text" placeholder="Enter Password" id="password" name="password" />
-                  <label htmlFor="copassword"> Confirm Password</label>
-                  <input type="text" placeholder="Enter Password again" id="copassword" name="copassword" />
-                  <h5><Link>Forgot password?</Link></h5>
-              </div>
-              <button>Sign Up</button>
-            </Form>
-            <div>
-                <h5>Or login using</h5>
-                <div>
-                    <Link><i></i></Link>
-                    <Link><i></i></Link>
-                    <Link><i></i></Link>
+        <div className="page-content">
+              <h2>SIGN UP</h2>
+              <Form>
+                <div className="inputs">
+                    <label htmlFor="email"> Email</label>
+                    <input type="text" placeholder="Enter E-mail" id="email" name="email" />
+                    <label htmlFor="password"> Password</label>
+                    <input type="text" placeholder="Enter Password" id="password" name="password" />
+                    <label htmlFor="copassword"> Confirm Password</label>
+                    <input type="text" placeholder="Enter Password again" id="copassword" name="copassword" />
+                    <h5><Link>Forgot password?</Link></h5>
                 </div>
-            </div>  
-            <h4>Already a member? <Link to="/log-in">Log In</Link></h4>
+                <button>Sign Up</button>
+              </Form>
+              <div>
+                  <h5>Or login using</h5>
+                  <div>
+                      <Link><i></i></Link>
+                      <Link><i></i></Link>
+                      <Link><i></i></Link>
+                  </div>
+              </div>  
+              <h4>Already a member? <Link to="/log-in">Log In</Link></h4>
+        </div>
       </div>
     </div>
   )
