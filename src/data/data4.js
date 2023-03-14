1678fb30bb7d269e51a551a9381eb94988c41c2f
MrLad01 <ladayodeji@gmail.com>
2023-03-14 21:01:54 +0100

    update

--- src/data/data4.js
+++ src/data/data4.js
@@ -11,7 +11,7 @@ const data4 = [
         author:"Karoline Red",
         occupation:"Chef",
         content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem rem ex nam, corporis repellat adipisci et quod non blanditiis maxime.",
-        rating: 4
+        rating: 4.0
     },
     {
         id:2,
@@ -19,7 +19,7 @@ const data4 = [
         author:"Collins Aloba",
         occupation:"Teacher",
         content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum placeat, officia quis nobis tempora est aliquid excepturi temporibus explicabo ut.",
-        rating: 3
+        rating: 3.0
     },
     {
         id:3,
@@ -27,7 +27,7 @@ const data4 = [
         author:"Alo Bosede",
         occupation:"Forex Trader",
         content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quam beatae illum, perspiciatis unde eius accusamus veniam architecto nobis deserunt!",
-        rating: 5
+        rating: 5.0
     }
     ,
     {
@@ -36,7 +36,7 @@ const data4 = [
         author:"Bill Brown",
         occupation:"Sales Manager",
         content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi facere iusto repudiandae nobis, cupiditate totam. Officia, nostrum? Ea, amet fugit.",
-        rating: 4
+        rating: 4.0
     } ,
     {
         id:5,
@@ -44,7 +44,7 @@ const data4 = [
         author:"Bill Brown",
         occupation:"Sales Manager",
         content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi facere iusto repudiandae nobis, cupiditate totam. Officia, nostrum? Ea, amet fugit.",
-        rating: 4
+        rating: 4.0
     }
 ]
 
