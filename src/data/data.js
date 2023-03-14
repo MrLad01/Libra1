7e7cf40612648024d9c1d887546ea79b0749cb6f
MrLad01 <ladayodeji@gmail.com>
2023-03-14 18:55:35 +0100

    update

--- src/data/data.js
+++ src/data/data.js
@@ -9,36 +9,42 @@ import book4 from "../images/image 8.png";
         image: book1,
         content:"Steal like an artist",
         author: "Austin Kleon",
+        rating: 4
     },
     {
         id:2,
         image: book2,
         content:"Half of a yellow sun",
         author: "Austin Kleon",
+        rating: 4
     },
     {
         id:3,
         image: book3,
         content:"The sun and her flowers",
         author: "Rupi Kaur",
+        rating: 4
     },
     {
         id:4,
         image: book4,
         content:"Purple Hibicus",
-        author: "Chimamanda Adichie"
+        author: "Chimamanda Adichie",
+        rating: 4
     },
     {
         id:5,
         image: book4,
         content:"Purple Hibicus",
-        author: "Chimamanda Adichie"
+        author: "Chimamanda Adichie",
+        rating: 4
     },
     {
         id:6,
         image: book4,
         content:"Purple Hibicus",
-        author: "Chimamanda Adichie"
+        author: "Chimamanda Adichie",
+        rating: 4
     }
 ]
 
