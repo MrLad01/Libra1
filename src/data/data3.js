890e9fe7c4b794bd105107b2a21cadfeda897d2d
MrLad01 <ladayodeji@gmail.com>
2023-03-14 18:57:57 +0100

    update

--- src/data/data3.js
+++ src/data/data3.js
@@ -9,36 +9,42 @@ const data3 = [
         image: book1,
         content:"The Alchemist",
         author: "Paulo Coelho",
+        rating: 4
     },
     {
         id:2,
         image: book2,
         content:"A confederacy of dunces",
         author: "John Kennedy T.",
+        rating: 4
     },
     {
         id:3,
         image: book3,
         content:"Why we sleep",
         author: "Matthew Walker",
+        rating: 4
     },
     {
         id:4,
         image: book4,
         content:"An enemy called average",
-        author: "John L.Mason"
+        author: "John L.Mason",
+        rating: 4
     },
     {
         id:5,
         image: book4,
         content:"An enemy called average",
-        author: "John L.Mason"
+        author: "John L.Mason",
+        rating: 4
     },
     {
         id:5,
         image: book4,
         content:"An enemy called average",
-        author: "John L.Mason"
+        author: "John L.Mason",
+        rating: 4
     }
 ]
 
