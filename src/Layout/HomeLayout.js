501fcaa85783a6034be59e034ac0c0111b98a61d
MrLad01 <ladayodeji@gmail.com>
2023-03-19 22:53:07 +0100

    update

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -8,18 +8,19 @@ export default function HomeLayout() {
   return (
     <div className="home">
       <div className="home1">
-        <motion.div 
-        variants={slideIn("left", "tween", 1, 2)}
-        className="home-text"
-        >
-          <p className="first-text">Studying books, a long lasting way to changing lives</p>
-          <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
-          <div>
-            <NavLink to="learn-more">Learn More</NavLink>
-            <NavLink to="contact">Contact Us</NavLink>
+        <motion.div variants={slideIn("left", "tween", 1, 2)}>
+          <div className="home-text">
+            <p className="first-text">Studying books, a long lasting way to changing lives</p>
+            <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
+            <div>
+              <NavLink to="learn-more">Learn More</NavLink>
+              <NavLink to="contact">Contact Us</NavLink>
+            </div>
           </div>
         </motion.div>
-        <motion.img  variants={slideIn("right", "tween", 1, 2)}  src={gof} alt="A girl reading" />
+        <motion.div  variants={slideIn("right", "tween", 1, 2)} >
+          <img src={gof} alt="A girl reading" />
+        </motion.div>
       </div>
 
     </div>
