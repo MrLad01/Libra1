376d2f21348ed4dc8cdf9e9975585476d305bc68
MrLad01 <ladayodeji@gmail.com>
2023-03-19 21:58:19 +0100

    update

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -7,14 +7,17 @@ export default function HomeLayout() {
   return (
     <div className="home">
       <div className="home1">
-        <div className="home-text">
+        <motion.div 
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="home-text"
+        >
           <p className="first-text">Studying books, a long lasting way to changing lives</p>
           <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
           <div>
             <NavLink to="learn-more">Learn More</NavLink>
             <NavLink to="contact">Contact Us</NavLink>
           </div>
-        </div>
+        </motion.div>
         <img src={gof} alt="A girl reading" />
       </div>
 
