100095ec6a733fae70980715bef0bf6164671b52
MrLad01 <ladayodeji@gmail.com>
2023-03-19 22:21:17 +0100

    update

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
 import gof from "../images/Rectangle 3.png"
 import { motion } from "framer-motion"
+import { slideIn } from "../utils/motion"
 
 
 export default function HomeLayout() {
@@ -8,10 +9,10 @@ export default function HomeLayout() {
     <div className="home">
       <div className="home1">
         <motion.div 
-        variants={slideIn('left', "tween", 0.2, 1)}
+        variants={slideIn("left", "tween", 1, 2)}
         className="home-text"
         >
-          <p className="first-text">Studying books, a long lasting way to changing lives</p>
+          <motion.p variants={slideIn("left", "tween", 1, 2)} className="first-text">Studying books, a long lasting way to changing lives</motion.p>
           <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
           <div>
             <NavLink to="learn-more">Learn More</NavLink>
