631cceee922a527f296a70f48101d3427274e77b
MrLad01 <ladayodeji@gmail.com>
2023-03-20 00:40:49 +0100

    update

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -12,9 +12,9 @@ export default function HomeLayout() {
         // variants={slideIn("left", "tween", 1, 2)}
         >
           <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            transition={{delay: 1, duration: 0.5}}
+            initial={{opacity: 0, x: '-100vw'}}
+            animate={{opacity: 1, x: 0}}
+            transition={{delay: 0.5, duration: 3, type: 'spring'}}
            className="home-text">
             <p className="first-text">Studying books, a long lasting way to changing lives</p>
             <p className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</p>
@@ -24,7 +24,11 @@ export default function HomeLayout() {
             </div>
           </motion.div>
         </motion.div>
-        <motion.div >  
+        <motion.div 
+          initial={{x: '100vw'}}
+          animate={{x: 0}}
+          transition={{delay: 0.5, duration: 10, type: 'spring', stiffness: 150}}
+        >  
         {/* //</div>variants={slideIn("right", "tween", 1, 2)} >// */}
           <img src={gof} alt="A girl reading" />
         </motion.div>
