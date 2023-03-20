91b6330d031920f47119ebedbba55a864223df2b
MrLad01 <ladayodeji@gmail.com>
2023-03-20 13:17:03 +0100

    update

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom"
 import gof from "../images/Rectangle 3.png"
 import { motion } from "framer-motion"
 import { slideIn } from "../utils/motion"
+import { textVariant } from "../utils/motion"
 
 
 export default function HomeLayout() {
@@ -19,13 +20,10 @@ export default function HomeLayout() {
            className="home-text">
             <motion.p className="first-text">Studying books, a long lasting way to changing lives</motion.p>
             <motion.p 
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
-              transition={{delay: 1 , duration: 1}}
+              variants={textVariant(0.5)}
               className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</motion.p>
             <motion.div
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
+              variants={textVariant(0.5)}
             >
               <NavLink to="learn-more">Learn More</NavLink>
               <NavLink to="contact">Contact Us</NavLink>
