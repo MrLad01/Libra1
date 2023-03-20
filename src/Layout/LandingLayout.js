ed53735e3fd41099ac8c50680fcafaf10b55184a
MrLad01 <ladayodeji@gmail.com>
2023-03-20 13:48:12 +0100

    update

--- src/Layout/LandingLayout.js
+++ src/Layout/LandingLayout.js
@@ -1,27 +1,31 @@
 import { NavLink, Outlet } from "react-router-dom";
 import Frame304 from '../images/Frame304.png';
+import { motion } from "framer-motion";
 
 export default function LandingLayout() {
   return (
     <div className="landing">
-      <header>
+      <motion.header
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{delay: 0.1, duration: 1}}
+      >
         <img src={Frame304} alt="logo" />
         <nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="services">Services</NavLink>
-            {/* <NavLink to="settings"> */}
                 <select name="Settings" id="" >
                     <option value="0">Settings</option>
                     <option value="">No Value</option>
                 </select>
-                {/* </NavLink> */}
+
             <NavLink to="help">Help</NavLink>
         </nav>
         <div>
             <NavLink to="sign-up">Sign Up</NavLink>
             <NavLink to="log-in">Log In</NavLink>
         </div>
-      </header>
+      </motion.header>
       <main>
         <Outlet />
       </main>
