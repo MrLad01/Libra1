8e5800b14b17aee1ba04e1030c70874cf1aadc2b
MrLad01 <ladayodeji@gmail.com>
2023-03-20 16:45:55 +0100

    update

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,27 +4,41 @@ import Cards from "../component/Cards";
 import data from "../data/data"
 import data2 from "../data/data2"
 import Item from "../component/Item";
+import { Navigation, Pagination, A11y, Keyboard, Mousewheel } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/mousewheel'
+
+
 
 export default function Services() {
 
   const items = data2.map(item => {
-    return (
-      <Item
-      key={item.id}
-      img={item.img}
-      title={item.title}
-      content={item.content}
-      />
+    return (  
+      <SwiperSlide>
+          <Item
+            key={item.id}
+            img={item.img}
+              title={item.title}
+              content={item.content}
+            />
+      </SwiperSlide> 
+ 
     )
   })
   const books = data.map(book => {
     return (
-      <Cards
-      key={book.id}
-      img={book.image}
-      content={book.content}
-      author={book.author}
-      rating={book.rating}
+      <Cards                 
+       key={book.id}
+       img={book.image}
+       content={book.content}
+       author={book.author}
+       rating={book.rating}
       />
       )
     })
@@ -33,16 +47,52 @@ export default function Services() {
       <div className="services">
       <div className="what-we-do">  
         <h3>What We Do</h3>
-        <div>
+        {/* <div>
           { items }
-        </div>
+        </div> */}
+        <Swiper
+           modules={[Navigation, Pagination, A11y, Keyboard, Mousewheel]}
+            spaceBetween={0}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            keyboard={{ enabled: true }}
+            mousewheel={{enabled: true}}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+         >
+            {/* <SwiperSlide>
+            { items }
+             </SwiperSlide> */}
+             {items}
+             
+        </Swiper>
       </div>
 
       <div className="trending">
         <h3>Trending Books</h3>
-        <div className="t-books">
+        {/* <div className="t-books"> */}
+
+        <Swiper
+           modules={[Navigation, Pagination, A11y]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+         >
+            <SwiperSlide>
+               {books}
+             </SwiperSlide>
+        </Swiper>
+        <div>
           {books}
         </div>
+
+        {/* </div> */}
+        {/* <div className="t-books">
+          {books}
+        </div> */}
 
         <div className="nav-buttons">
           <NavLink><div></div></NavLink>
