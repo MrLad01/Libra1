d0391c96f97a6ebfd77d9c2c1ef8ec5714c89e42
MrLad01 <ladayodeji@gmail.com>
2023-03-21 09:09:42 +0100

    update

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -26,32 +26,34 @@ export default function LogIn() {
   return (
     <div>
       <img src="" alt="" />
-      <div className="form-container">
-        <div className="page-content">
-              <h2>LOG IN</h2>
-              <Form >
-                <div className="inputs">
-                    <label htmlFor="email"> Email</label>
-                    <input type="email" placeholder="Enter E-mail" id="email" name="email" required  />
-                    <label htmlFor="password"> Password</label>
-                    <input type="password" placeholder="Enter Password" id="password" name="password" required />
-                    <h5><Link>Forgot password?</Link></h5>
-                </div>
-                <button className="button1">Log In</button>
-                {/* {email}
-                {password} */}
-              </Form>
-              <div>
-                  <h5>Or login using</h5>
-                  <div className="login-icons">
-                      <Link to="/"><img src={facebook} alt="" /></Link>
-                      <Link to="/"><img src={google} alt="" /></Link>
-                      <Link to="/"><img src={twitter} alt="" /></Link>
-                  </div>
-              </div>  
-              <h4>New member? <Link to="/sign-up">Sign Up</Link></h4>
+      <div className="background ">
+          <div className="form-container">
+            <div className="page-content">
+                  <h2>LOG IN</h2>
+                  <Form >
+                    <div className="inputs">
+                        <label htmlFor="email"> Email</label>
+                        <input type="email" placeholder="Enter E-mail" id="email" name="email" required  />
+                        <label htmlFor="password"> Password</label>
+                        <input type="password" placeholder="Enter Password" id="password" name="password" required />
+                        <h5><Link>Forgot password?</Link></h5>
+                    </div>
+                    <button className="button1">Log In</button>
+                    {/* {email}
+                    {password} */}
+                  </Form>
+                  <div>
+                      <h5>Or login using</h5>
+                      <div className="login-icons">
+                          <Link to="/"><img src={facebook} alt="" /></Link>
+                          <Link to="/"><img src={google} alt="" /></Link>
+                          <Link to="/"><img src={twitter} alt="" /></Link>
+                      </div>
+                  </div>  
+                  <h4>New member? <Link to="/sign-up">Sign Up</Link></h4>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,32 +8,34 @@ export default function SignUp() {
   return (
     <div>
       <img src="" alt="" />
-      <div className="form-container">
-        <div className="page-content">
-              <h2>SIGN UP</h2>
-              <Form>
-                <div className="inputs">
-                    <label htmlFor="email"> Email</label>
-                    <input type="email" placeholder="Enter E-mail" id="email" name="email" required />
-                    <label htmlFor="password"> Password</label>
-                    <input type="password" placeholder="Enter Password" id="password" name="password" required />
-                    <label htmlFor="copassword"> Confirm Password</label>
-                    <input type="password" placeholder="Enter Password again" id="copassword" name="copassword" required />
-                    {/* <h5><Link>Forgot password?</Link></h5> */}
-                </div>
-                <button className="button2">Sign Up</button>
-              </Form>
-              <div>
-                  <h5>Or login using</h5>
-                  <div className="login-icons2">
-                      <Link to="/"><img src={facebook} alt="" /></Link>
-                      <Link to="/"><img src={google} alt="" /></Link>
-                      <Link to="/"><img src={twitter} alt="" /></Link>
-                  </div>
-              </div>  
-              <h4>Already a member? <Link to="/log-in">Log In</Link></h4>
-        </div>
+      <div className="background">
+            <div className="form-container">
+              <div className="page-content">
+                    <h2>SIGN UP</h2>
+                    <Form>
+                      <div className="inputs">
+                          <label htmlFor="email"> Email</label>
+                          <input type="email" placeholder="Enter E-mail" id="email" name="email" required />
+                          <label htmlFor="password"> Password</label>
+                          <input type="password" placeholder="Enter Password" id="password" name="password" required />
+                          <label htmlFor="copassword"> Confirm Password</label>
+                          <input type="password" placeholder="Enter Password again" id="copassword" name="copassword" required />
+                          {/* <h5><Link>Forgot password?</Link></h5> */}
+                      </div>
+                      <button className="button2">Sign Up</button>
+                    </Form>
+                    <div>
+                        <h5>Or login using</h5>
+                        <div className="login-icons2">
+                            <Link to="/"><img src={facebook} alt="" /></Link>
+                            <Link to="/"><img src={google} alt="" /></Link>
+                            <Link to="/"><img src={twitter} alt="" /></Link>
+                        </div>
+                    </div>  
+                    <h4>Already a member? <Link to="/log-in">Log In</Link></h4>
+              </div>
+            </div>
+          </div>
       </div>
-    </div>
   )
 }
