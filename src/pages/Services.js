5303369e6de70415322f9e60d4d865b47b0ef01e
MrLad01 <ladayodeji@gmail.com>
2023-03-22 13:22:25 +0100

    update

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -83,7 +83,12 @@ export default function Services() {
         </motion.div>
       </div>
 
-      <motion.div className="trending">
+      <motion.div 
+      className="trending"
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{duration: 0.5, delay: 0.3}}
+      >
         <h3>Trending Books</h3>
   
         <Swiper
