93eafbd452202b95d251996daffaa985d1b43e47
MrLad01 <ladayodeji@gmail.com>
2023-03-30 18:32:36 +0100

    update

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -44,8 +44,8 @@ export default function SignUp() {
                           <input type={view ?"password":"text"} placeholder="Enter Password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)} /> 
                           <label htmlFor="copassword"> Confirm Password</label>
                           <FontAwesomeIcon icon="fa-solid fa-unlock" style={{position: "relative", top: "39px", right: "204px", color: "#AEADAE"}} />
-                          { view2 && <FontAwesomeIcon icon="fa-solid fa-eye" style={{position: "relative", top: "43px", left: "167px", color: "#AEADAE", cursor: "pointer"}} onClick={handleView2} />}
-                          { !view2 && <FontAwesomeIcon icon="fa-solid fa-eye-slash" style={{position: "relative", top: "43px", left: "167px", color: "#AEADAE", cursor: "pointer"}} onClick={handleView2} />}
+                          { view2 && <FontAwesomeIcon icon="fa-solid fa-eye" style={{position: "relative", top: "43px", left: "167px", color: "#AEADAE", cursor: "pointer"}} onClick={() => setView2(!view2)} />}
+                          { !view2 && <FontAwesomeIcon icon="fa-solid fa-eye-slash" style={{position: "relative", top: "43px", left: "167px", color: "#AEADAE", cursor: "pointer"}} onClick={() => setView2(!view2)} />}
                           <input type={view2 ?"password":"text"} placeholder="Enter Password again" id="copassword" name="copassword" required onChange={(e) => setNewPassword(e.target.value)} />
                       </div>
                       <button className="button2">Sign Up</button>
