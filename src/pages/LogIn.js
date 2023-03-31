0c7d1d3208f6965d3712e23d7a61bef28997d241
MrLad01 <ladayodeji@gmail.com>
2023-03-31 22:32:57 +0100

    update

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -22,7 +22,7 @@ export default function LogIn() {
   }
   return (
         <div>
-          <img src="" alt="" />
+          <img src={ logo } alt="logo" />
           <div className="background ">
               <div className="form-container">
                 <div className="page-content">
