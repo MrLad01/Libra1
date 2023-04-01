35800d5d89edc1cd6342de8d004d210a190fb54b
MrLad01 <ladayodeji@gmail.com>
2023-04-01 22:55:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import LogIn from "./pages/LogIn";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
-// import { faTwitter, faFontAwesome, faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { AnimatePresence } from "framer-motion";
 
