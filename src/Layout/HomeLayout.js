3cf11db2fc870553fe687f41fb7742bf0feb778b
MrLad01 <ladayodeji@gmail.com>
2023-04-29 22:55:30 +0100

    update

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -32,11 +32,11 @@ export default function HomeLayout() {
           <p><motion.span
           initial={{opacity: 0}}
           whileInView={{opacity: 1}}
-          transition={{duration: 2, delay: 0.5}}
+          transition={{duration: 1, delay: 0.25}}
           >Sorry your screen size is small,</motion.span> <motion.span
               initial={{opacity: 0}}
               whileInView={{opacity: 1}}
-              transition={{duration: 2, delay: 2}}
+              transition={{duration: 1, delay: 1}}
           >but in the coming days we'll get something appropriate for you</motion.span></p>
         </div>
         ) : (
@@ -45,20 +45,20 @@ export default function HomeLayout() {
           <motion.div 
           exit={{
             opacity: 0,
-            transition: {duration: 1, delay: 0.1}}}
+            transition: {duration: 0.5, delay: 0.05}}}
           >
             <motion.div
-              variants={slideIn("left", "tween", 0.5, 3)}
+              variants={slideIn("left", "tween", 0.25, 1.5)}
               initial="hidden"
               animate="show"
 
             className="home-text">
               <motion.p className="first-text">Studying books, a long lasting way to changing lives</motion.p>
               <motion.p 
-                variants={textVariant(0.2)}
+                variants={textVariant(0.1)}
                 className="second-text">We relieve you to a great deal by sourcing books for you to lend, read and change your lives. We are always ready to help</motion.p>
               <motion.div
-                variants={buttonVariant(6)}
+                variants={buttonVariant(3)}
                 initial="hidden"
                 animate="show"
               >
@@ -68,7 +68,7 @@ export default function HomeLayout() {
             </motion.div>
           </motion.div>
           <motion.div 
-            variants={slideIn("right", "spring", 0.5, 4)}
+            variants={slideIn("right", "spring", 0.25, 2)}
             initial="hidden"
             animate="show"
           >  
